f24d047df156881a6cb4a9f86ac8f61787f66a29
Dragos Nedelcu <dragos.nedelcu@lokalleads.de>
2017-08-28 19:52:13 +0200

    setted up grapql setupt

--- server/graphqlSetup.js
+++ server/graphqlSetup.js
@@ -1,2 +1,52 @@
 import {makeExecutableSchema} from 'graphql-tools'
-import {grapqlExpress, graphiqlExpress} from 'apollo-server-express'
+import {graphqlExpress, graphiqlExpress} from 'apollo-server-express'
+
+const typeDefs = [
+  `
+    type Student {
+      id: ID!
+      name: String!
+      age: Integer!
+    }
+    type Query {
+      allStudents: [Student!]
+    }
+    schema {
+      query: Query
+    }
+  `
+]
+
+const students = [
+  {
+    id: 1,
+    name: "Olga",
+    age: 15
+  },
+  {
+    id: 2,
+    name: "Miruna",
+    age: 15
+  }
+]
+
+const resolvers = {
+  Query: {
+    allStudents: () => students
+  }
+}
+
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers
+})
+
+export default (app) => {
+  app.use('/graphql', graphqlExpress(() => {
+    return {
+      schema
+    }
+  }
+  ))
+  app.use('/graphiql', graphiqlExpress({endpointURL: "/graphql"}))
+}
