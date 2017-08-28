62f9f9ae5d4871bb963a0923eb2176cc8478f4e0
Dragos Nedelcu <dragos.nedelcu@lokalleads.de>
2017-08-28 20:08:25 +0200

    configuring webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,37 @@
-module.exports = {
-  devtool: 'inline-source-map',
-  entry: [
-    './client/src/index.js'
-  ],
-  output: {
-    path: __dirname,
-    publicPath: '/',
-    filename: 'bundle.js'
+import path from 'path'
+import webpack from 'webpack'
+import reloadable from "express-reloadable"
+import HtmlWebpackPlugin from "html-webpack-plugin"
+import CompressionWebpackPlugin from "compression-webpack-plugin"
+import AssetsPlugin from 'assets-webpack-plugin'
+
+const isDev = true,
+      [host,port] = 80
+
+const vendor = [
+  "react",
+  "react-dom",
+  "react-apollo",
+  "redux",
+  "react-redux",
+  "redux-first-router",
+  "history",
+  "recompose"
+]
+
+export default{
+  devtool: isDev ? "inline-source-map" : "hidden-source-map",
+  bail: false,
+  node: {
+    fs: false,
+    process: false,
+    Buffer: false
   },
-  module: {
-    loaders: [{
-      exclude: /node_modules/,
-      loader: 'babel',
-      query: {
-        presets: ['react', 'es2015', 'stage-1']
-      }
-    }]
-  },
-  resolve: {
-    extensions: ['', '.js', '.jsx']
-  },
-  devServer: {
-    historyApiFallback: true,
-    contentBase: './'
+  entry: isDev ? {
+    app: [
+      'babel-polyfill',
+      'react-hot-loader/patch'
+      `webpack-dev-server/client?https://${host}:${port}`
+    ]
   }
-};
+}
